c93ef1cb9fd10f2384787ac7763bc159c095dc88
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-04 23:12:02 -0700

    import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BroserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <Router>
-      <Route path='/' exact component={Join} />
-      <Route path='/chat' exact component={Chat} />
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" exact component={Chat} />
     </Router>
-    );
+  );
 }
 
 export default App;
