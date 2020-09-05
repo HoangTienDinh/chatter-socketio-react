78dca26b74f5a55fac4c3a1271b22ad431c31c0b
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-04 23:09:37 -0700

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import { BroserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
