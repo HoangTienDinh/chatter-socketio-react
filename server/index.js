ce70a85c303b7df37a1b2e4c89f62096afae1475
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-05 21:57:40 -0700

    export router and import it

--- server/index.js
+++ server/index.js
@@ -4,13 +4,10 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
-
-
-
-
 
 server.listen(PORT, () => console.log(`Listeing on port ${PORT}`));
