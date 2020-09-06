868357bb4551b78905d6b3579e376ab3ac6f6d0c
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-05 22:08:40 -0700

    finish login layout

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -10,12 +10,16 @@ const Join = () => {
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
         <div>
-          <input placeholder="" className="joinInput" type="text" onChange={} />
+          <input placeholder="Name" className="joinInput" type="text" onChange={} />
         </div>
         <div>
-          <input placeholder="" className="joinInput" type="text" onChange={} />
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={} />
         </div>
-        
+        <Link>
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
       </div>
     </div>
   );
