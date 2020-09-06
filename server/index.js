e6f9a854d04e0beaddd0350d51ddb922ab0416aa
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-05 22:01:07 -0700

    simple io connect/disconnect message

--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
 
+// SETUP FOR SERVER AND SOCKETIO CONNECTION
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+// Code for connects and disconnects
+io.on('connection', (socket) => {
+    console.log('we have a new connection.')
+
+    socket.on('disconnect', () => {
+        console.log('User has left.')
+    })
+});
+
+// SETUP FOR MIDDLEWARE
+app.use(router)
 
 server.listen(PORT, () => console.log(`Listeing on port ${PORT}`));
