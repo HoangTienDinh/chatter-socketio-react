5de13111411cfade782cff741b839dab9317f4f3
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-05 21:54:54 -0700

    get server running

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ const express = require("express");
 const socketio = requite("socket.io");
 const http = require("http");
 
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
+
+
+
+server.listen(PORT, () => console.log(`Listeing on port ${PORT}`));
