ca21e71507ccb7f3e6702a12562a8f0409e14566
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-06 14:00:57 -0700

    start socket conneciton and set name/room

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,13 +2,25 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+let socket;
+
 const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
   // useEffect is used for lifecycle functions
   useEffect(() => {
-      const data = queryString.parse(location.search)
-      console.log(data)
-      console.log(location.search)
-  }, []);
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+
+
+  }, [ENDPOINT, location.search]);
 
   return (
     <div>
