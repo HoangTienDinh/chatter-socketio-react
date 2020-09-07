3bc041a5e035ec19817c7bc8772baf45167a7863
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-07 15:37:50 -0700

    fix errors that prevented message send

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,72 +1,67 @@
 import React, { useState, useEffect } from "react";
-import queryString from "query-string";
+import queryString from 'query-string';
 import io from "socket.io-client";
-import InfoBar from "../InfoBar/InfoBar";
-import Input from "../Input/Input";
-import Messages from "../Messages/Messages";
 
-import "./Chat.css";
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
-  const [message, setMessage] = useState([]);
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
-  const ENDPOINT = "localhost:5000";
-
-  // useEffect is used for lifecycle functions
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name)
 
-    socket.emit("join", { name, room }, () => {});
-
-    return () => {
-      socket.emit("disconnect");
-
-      // turn off the one instance
-      socket.off();
-    };
-  }, [ENDPOINT, location.search]);
-
-  // for changes everytime a user sends a message
-  useEffect(() => {
-    socket.on("message", (message) => {
-      setMessages([...messages, message]);
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-  }, [messages]);
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
 
-  // on send, message gets sent to all users
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if (message) {
-      socket.emit("sendMessage", message, () => setMessage(""));
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
     }
-  };
-
-  console.log(message, messages);
+  }
 
   return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users}/>
     </div>
   );
-};
+}
 
 export default Chat;
--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "./Messages.css";
-import Message from "../Message/Message";
 
 import ScrollToBottom from "react-scroll-to-bottom";
 
-const Messages = ({ messages, name }) => {
-  return (
-    <ScrollToBottom>
-      {messages.map((message, i) => (
-        <div key={i}>
-          <Message message={message} name={name} />
-        </div>
-      ))}
-    </ScrollToBottom>
-  );
-};
+import Message from "./Message/Message";
+
+import "./Messages.css";
+
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom className="messages">
+    {messages.map((message, i) => (
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    ))}
+  </ScrollToBottom>
+);
 
 export default Messages;
