c8a78681bccf3bd2ab8e6971a96384c6af29c348
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-07 14:18:15 -0700

    build out jsx in infobar

---
+++ client/src/components/InforBar/InforBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./InfoBar.css";
+
+const InforBar = () => {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="online image" />
+        <h3>roomName</h3>
+      </div>
+
+      <div className="rightInnerContainer">
+        <a href="/">
+          <img src={closeIcon} alt="close image" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default InforBar;
